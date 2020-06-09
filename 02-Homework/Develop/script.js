64702a531e2958ab49e900bb34b504d3dfb6956a
Preston Bradford <psbradford4100@att.net>
2020-06-08 22:40:47 -0400

    finished the porject in one go like a moron

---
+++ 02-Homework/Develop/script.js
@@ -0,0 +1,142 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  function generatePassword() {
+    // promts user to choose pass length and parses into an integer
+    var passLength = prompt("Password length (min:8 chars, max:128 chars): ");
+    var length = parseInt(passLength);
+
+    var passChars = prompt("What type of characters would you like to use: \n(select them all desired characters types at once with spaces between types:\nlowercase, uppercase, numeric, special characters or all");
+    var charset;
+    var charset1 = "abcdefghijklmnopqrstuvwxyz";
+    var charset2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var charset3 = "0123456789";
+    var charset4 = "!@#$%&*";
+    var charset5 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*"
+
+    if (length >= 8 && length <= 128) {
+      length;
+      // straight up just the character set to choose from
+      // The retVal kinda sets it up as both a split and gets returned as a string for the password
+      if (passChars === "lowercase") {
+        charset = charset1;
+        retVal = "";
+        for (var i = 0, n = charset.length; i < length; ++i) {
+          //parses and prints like an array after choosing random whole numbers associated with the charlist
+          retVal += charset.charAt(Math.floor(Math.random() * n));
+        }
+        return retVal;
+      }
+      else if (passChars === "uppercase") {
+        charset = charset2;
+        retVal = "";
+        for (var i = 0, n = charset.length; i < length; ++i) {
+          //parses and prints like an array after choosing random whole numbers associated with the charlist
+          retVal += charset.charAt(Math.floor(Math.random() * n));
+        }
+        return retVal;
+      }
+      else if (passChars === "numeric") {
+        charset = charset3;
+        retVal = "";
+        for (var i = 0, n = charset.length; i < length; ++i) {
+          //parses and prints like an array after choosing random whole numbers associated with the charlist
+          retVal += charset.charAt(Math.floor(Math.random() * n));
+        }
+        return retVal;
+      }
+      else if (passChars === "special characters" || passChars === "special") {
+        charset = charset4;
+        retVal = "";
+        for (var i = 0, n = charset.length; i < length; ++i) {
+          //parses and prints like an array after choosing random whole numbers associated with the charlist
+          retVal += charset.charAt(Math.floor(Math.random() * n));
+        }
+        return retVal;
+      }
+      else if (passChars === "all") {
+        charset = charset5;
+        retVal = "";
+        for (var i = 0, n = charset.length; i < length; ++i) {
+          //parses and prints like an array after choosing random whole numbers associated with the charlist
+          retVal += charset.charAt(Math.floor(Math.random() * n));
+        }
+        return retVal;
+      }
+      else {
+        alert("Please enter valid response")
+      }
+    }
+
+    //  I honestly couldnt figure out how to do this part of connecting two inputs and thier character sets, lemme know if I was on the right track here
+    // Didnt want to code every variation and couldnt figureout how to do more than one variable
+
+    // else if (passChars === "lowercase uppercase") {
+    //   charset = charset1 + charset2;
+    //   retVal = "";
+    //   for (var i = 0, n = charset.length; i < length; ++i) {
+    //     //parses and prints like an array after choosing random whole numbers associated with the charlist
+    //     retVal += charset.charAt(Math.floor(Math.random() * n));
+    //   }
+    //   return retVal;
+    // }
+    // else if (passChars === "lowercase numeric") {
+    //   charset = charset1 + charset3;
+    //   retVal = "";
+    //   for (var i = 0, n = charset.length; i < length; ++i) {
+    //     //parses and prints like an array after choosing random whole numbers associated with the charlist
+    //     retVal += charset.charAt(Math.floor(Math.random() * n));
+    //   }
+    //   return retVal;
+    // }
+    // else if (passChars === "lowercase special characters" || passChars === "lowercase special") {
+    //   charset = charset1 + charset3;
+    //   retVal = "";
+    //   for (var i = 0, n = charset.length; i < length; ++i) {
+    //     //parses and prints like an array after choosing random whole numbers associated with the charlist
+    //     retVal += charset.charAt(Math.floor(Math.random() * n));
+    //   }
+    //   return retVal;
+    // }
+    // else if (passChars === "uppercase numeric") {
+    //   charset = charset1 + charset3;
+    //   retVal = "";
+    //   for (var i = 0, n = charset.length; i < length; ++i) {
+    //     //parses and prints like an array after choosing random whole numbers associated with the charlist
+    //     retVal += charset.charAt(Math.floor(Math.random() * n));
+    //   }
+    //   return retVal;
+    // }
+    // else if (passChars === "uppercase special characters" || passChars === "uppercase special") {
+    //   charset = charset1 + charset3;
+    //   retVal = "";
+    //   for (var i = 0, n = charset.length; i < length; ++i) {
+    //     //parses and prints like an array after choosing random whole numbers associated with the charlist
+    //     retVal += charset.charAt(Math.floor(Math.random() * n));
+    //   }
+    //   return retVal;
+    // }
+    // else if (passChars === "lowercase numeric") {
+    //   charset = charset1 + charset3;
+    //   retVal = "";
+    //   for (var i = 0, n = charset.length; i < length; ++i) {
+    //     //parses and prints like an array after choosing random whole numbers associated with the charlist
+    //     retVal += charset.charAt(Math.floor(Math.random() * n));
+    //   }
+    //   return retVal;
+    // }
+
+    // else {
+    //   alert("Please input a vaild length.")
+    // }
+  }
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Password-Generation
+HW-3 of GT BootCamp
+Preston Bradford
+
+MIT License
+
+Copyright (c) <2020> <Preston Bradford>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+The reason behind this program's creation is to further my studies during my Georgia Tech BootCamp, this one particularly intrested me since I like the idea of making a security interface and the like. It was really good practice, however I am still lacking in the ability to work around hard coding and combining outcomes and inputs properly. 
+For this program I created a password generator from the outlines and README given for the task.
+I look forward to more challenges and some good input from the grading system as to how I can better this program in the near future.
